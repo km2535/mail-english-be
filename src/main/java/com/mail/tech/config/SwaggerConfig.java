2be1bc46fcf90a36945a0b108ceef952493fd750
Lee kangmin <km2535@naver.com>
2025-01-05 14:34:38 +0900

    feat: mail-english

---
+++ src/main/java/com/mail/tech/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.mail.tech.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.swagger.v3.core.jackson.ModelResolver;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI openAPI() {
+		return new OpenAPI()
+			.info(apiInfo());
+	}
+
+	private	 Info apiInfo() {
+		return new Info()
+			.title("매일 잉글리쉬 API 명세서")
+			.description("매일 잉글리쉬 API 명세서")
+			.version("1.0.0");
+	}
+
+	@Bean
+	public ModelResolver modelResolver(ObjectMapper objectMapper) {
+		return new ModelResolver(objectMapper);
+	}
+}
